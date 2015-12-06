671f8ad4a805f3f014d2d17a41e31f2fd61438b1
JeongMinSik <jms6025a@naver.com>
2015-12-06 13:07:23 +0900

    도움말추가_버전1.0

--- title_state.py
+++ title_state.py
@@ -5,10 +5,10 @@ import class_cursor
 
 name = "TitleState"
 
-NEW_GAME, LOAD_GAME = 1,2
+NEW_GAME, LOAD_GAME, HELP = 1, 2, 3
 
 def enter(object1=None,object2=None):
-    global back_image,space_image, name_image, opacify_time,change, name_x, cursor,bgm,select_menu, newgame_image, loadgame_image
+    global back_image,space_image, name_image, opacify_time,change, name_x, cursor,bgm,select_menu, newgame_image, loadgame_image, help_image, readme_image,ishelp
     bgm=load_music('Sound/title_bgm.mp3')
     bgm.set_volume(40)
     bgm.repeat_play()
@@ -18,8 +18,12 @@ def enter(object1=None,object2=None):
     name_image = load_image('Title/title_name_2.png')
     newgame_image = load_image('Title/newgame.png')
     loadgame_image = load_image('Title/loadgame.png')
+    help_image = load_image('Title/help.png')
+    readme_image = load_image('Title/readme.png')
+    readme_image.opacify(0.9)
 
     select_menu = False
+    ishelp = False
 
     opacify_time, change=0, 1
     name_x = 1500
@@ -28,8 +32,8 @@ def enter(object1=None,object2=None):
 
 def exit():
     print("타이틀 exit")
-    global back_image,space_image, name_image
-    del(back_image,space_image, name_image)
+    global back_image,space_image, name_image, help_image, readme_image
+    del(back_image,space_image, name_image, readme_image)
 
 
 def pause():
@@ -47,13 +51,14 @@ def check_click(x,y):
     if x < 850 : return False
     if x > 1150 : return False
     if y > 400: return False
-    if y < 170: return False
+    if y < 35: return False
     if y > 300: return NEW_GAME
-    if y < 270: return LOAD_GAME
+    if y < 270 and y > 160: return LOAD_GAME
+    if y < 140: return HELP
     return False
 
 def handle_events(frame_time):
-    global name_x, select_menu
+    global name_x, select_menu, ishelp
     events=get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -61,9 +66,16 @@ def handle_events(frame_time):
         elif(event.type == SDL_MOUSEMOTION):
             class_cursor.Cursor.x,class_cursor.Cursor.y=event.x,600-event.y
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button ==SDL_BUTTON_LEFT:
-            if check_click(event.x,600-event.y) and select_menu == True:
-                game_framework.push_state(main_state,check_click(event.x,600-event.y))
+            ishelp = False
+            if select_menu == True:
+                click_type = check_click(event.x,600-event.y)
+                if click_type == NEW_GAME or click_type == LOAD_GAME:
+                    game_framework.push_state(main_state,click_type)
+                if click_type == HELP:
+                    ishelp = True
         else:
+            if(event.type) == (SDL_KEYDOWN):
+                ishelp=False
             if(event.type, event.key) == (SDL_KEYDOWN,SDLK_ESCAPE):
                 game_framework.quit()
             elif(event.type, event.key) == (SDL_KEYDOWN,SDLK_SPACE):
@@ -96,8 +108,11 @@ def draw(frame_time):
         if select_menu == False:
             space_image.draw(900,60)
         else:
+            if ishelp:
+                readme_image.draw(370,240)
             newgame_image.draw(1000,350)
             loadgame_image.draw(1000,220)
+            help_image.draw(1000,90)
     cursor.draw()
     update_canvas()
 
